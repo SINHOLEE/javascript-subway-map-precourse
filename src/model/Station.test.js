487afa14242cd2ac9f26acf7c3ed63cf77923152
이신호 <dltlsgh5@naver.com>
2020-12-11 22:59:34 +0900

    test: Station Model unit test 구현

--- src/model/Station.test.js
+++ src/model/Station.test.js
@@ -1,6 +1,36 @@
-import { StationModel } from "./Station.js";
-const stationModel = StationModel();
+import StationModel from "./Station.js";
+import { genUUID } from "./utils";
 
-it("잘 생성되었나?", () => {
-	expect(stationModel.getStations()).toBe([]);
+describe("station Model", () => {
+	test("잘 생성되었나?", () => {
+		const stationModel = new StationModel();
+		expect(stationModel.getStations()).toStrictEqual([]);
+	});
+
+	test("잘 추가 되나", () => {
+		const stationModel = new StationModel();
+		const id = genUUID();
+		const newStation = { name: "선릉", id };
+		stationModel.addStation(newStation);
+		expect(stationModel.getStations()).toStrictEqual([newStation]);
+	});
+	test("잘 삭제 되나", () => {
+		const stationModel = new StationModel();
+		const id = genUUID();
+		const newStation = { name: "선릉", id };
+		stationModel.addStation(newStation);
+
+		stationModel.removeStationById(id);
+		expect(stationModel.getStations()).toStrictEqual([]);
+	});
+	test("없는 id로 삭제하면 잘 삭제가 되나?", () => {
+		const stationModel = new StationModel();
+		const id = genUUID();
+		const newStation = { name: "선릉", id };
+		stationModel.addStation(newStation);
+		const notExistId = "123";
+		stationModel.removeStationById(notExistId);
+
+		expect(stationModel.getStations()).toStrictEqual([newStation]);
+	});
 });
