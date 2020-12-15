f44b6344ad5926c0ee8382027545064174ca09b9
sinho <dltlsgh5@gmail.com>
2020-12-15 21:12:08 +0900

    refactor: localstorage 독립성 문제 해결

--- src/model/Station.test.js
+++ src/model/Station.test.js
@@ -2,45 +2,48 @@ import StationModel from "./Station.js";
 import {genUUID} from "../../utils.js";
 
 describe("station Model", () => {
-	test("잘 생성되었나?", () => {
+	beforeEach(() => {
+		localStorage.clear();
+	});
+	it("잘 생성되었나?", () => {
 		const stationModel = new StationModel();
 		expect(stationModel.getStations()).toStrictEqual([]);
 	});
 
-	test("잘 추가 되나", () => {
+	it("잘 추가 되나", () => {
 		const stationModel = new StationModel();
 		const id = genUUID();
 		const newStation = {name: "선릉", id};
 		stationModel.addStation(newStation);
 		expect(stationModel.getStations()).toStrictEqual([newStation]);
 	});
-	// test("잘 추가 되나2", () => {
-	// 	const stationModel = new StationModel();
-	// 	const id = genUUID();
-	// 	const newStation = {name: "선릉", id};
-	// 	stationModel.addStation(newStation);
-	// 	const id1 = genUUID();
-	// 	const newStation1 = {name: "시흥", id1};
-	// 	stationModel.addStation(newStation1);
-	// 	expect(stationModel.getStations()).toStrictEqual([newStation, newStation1]);
-	// });
-	// test("잘 삭제 되나", () => {
-	// 	const stationModel = new StationModel();
-	// 	const id = genUUID();
-	// 	const newStation = {name: "선릉", id};
-	// 	stationModel.addStation(newStation);
+	it("잘 추가 되나2", () => {
+		const stationModel = new StationModel();
+		const id = genUUID();
+		const newStation = {name: "선릉", id};
+		stationModel.addStation(newStation);
+		const id1 = genUUID();
+		const newStation1 = {name: "시흥", id1};
+		stationModel.addStation(newStation1);
+		expect(stationModel.getStations()).toStrictEqual([newStation, newStation1]);
+	});
+	it("잘 삭제 되나", () => {
+		const stationModel = new StationModel();
+		const id = genUUID();
+		const newStation = {name: "선릉", id};
+		stationModel.addStation(newStation);
 
-	// 	stationModel.removeStationById(id);
-	// 	expect(stationModel.getStations()).toStrictEqual([]);
-	// });
-	// test("없는 id로 삭제하면 잘 삭제가 되나?", () => {
-	// 	const stationModel = new StationModel();
-	// 	const id = genUUID();
-	// 	const newStation = {name: "선릉", id};
-	// 	stationModel.addStation(newStation);
-	// 	const notExistId = "123";
-	// 	stationModel.removeStationById(notExistId);
+		stationModel.removeStationById(id);
+		expect(stationModel.getStations()).toStrictEqual([]);
+	});
+	it("없는 id로 삭제하면 잘 삭제가 되나?", () => {
+		const stationModel = new StationModel();
+		const id = genUUID();
+		const newStation = {name: "선릉", id};
+		stationModel.addStation(newStation);
+		const notExistId = "123";
+		stationModel.removeStationById(notExistId);
 
-	// 	expect(stationModel.getStations()).toStrictEqual([newStation]);
-	// });
+		expect(stationModel.getStations()).toStrictEqual([newStation]);
+	});
 });
