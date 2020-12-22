c4168703a9d8e9bcf312c2bef3a0c2a8106d24c2
이신호 <dltlsgh5@naver.com>
2020-12-22 22:29:38 +0900

    refactor: view 수정

--- src/view/LineView.js
+++ src/view/LineView.js
@@ -1,110 +1,112 @@
 export default class LineView {
-  constructor() {
-    this.$linesTab = null;
-    this.$form = null;
-    this.$input = null;
-    this.$table = document.createElement("table");
-    this.$tbody = null;
-  }
-  //공통
-  setLinesTab($linesTab) {
-    this.$linesTab = $linesTab;
-  }
-  //공통
-  clearInputValue() {
-    if (this.$input) {
-      this.$input.value = "";
-    }
-  }
-  //공통
-  _clearTable() {
-    if (this.$table) {
-      this.$table.remove();
-    }
-  }
-  //공통
-  _clearLinesTab() {
-    if (this.$linesTab) {
-      this.$linesTab.innerHTML = "";
-    }
-  }
-  showTab() {
-    this.$linesTab.style.display = "block";
-  }
+	constructor() {
+		this.$linesTab = null;
+		this.$input = null;
+		this.$table = document.createElement("table");
+		this.$tbody = null;
+	}
+	//공통
+	setLinesTab($linesTab) {
+		this.$linesTab = $linesTab;
+	}
+	//공통
+	clearInputValue() {
+		if (this.$input) {
+			this.$input.value = "";
+		}
+	}
+	//공통
+	_clearTable() {
+		if (this.$table) {
+			this.$table.remove();
+		}
+	}
+	//공통
+	_clearLinesTab() {
+		if (this.$linesTab) {
+			this.$linesTab.innerHTML = "";
+		}
+	}
+	showTab() {
+		this.$linesTab.style.display = "block";
+	}
 
-  //공통
-  _createInput() {
-    const $label = document.createElement("label");
-    $label.htmlFor = "line-name-input";
-    $label.innerText = "노선 이름";
+	//공통
+	_createInput() {
+		const $label = this._createLabel("line-name-input", "노선 이름");
+		const $input = document.createElement("input");
+		$input.id = "line-name-input";
+		$input.name = "name";
+		$input.placeholder = "노선 이름을 입력해 주세요.";
+		this.$input = $input;
+		const div = document.createElement("div");
+		div.append($label);
+		div.append($input);
+		return div;
+	}
+	_createLabel(tagId, labelInnerText) {
+		const $label = document.createElement("label");
+		$label.htmlFor = tagId;
+		$label.innerText = labelInnerText;
+		return $label;
+	}
 
-    const $input = document.createElement("input");
-    $input.id = "line-name-input";
-    $input.name = "name";
-    $input.placeholder = "노선 이름을 입력해 주세요.";
-    this.$input = $input;
-    const div = document.createElement("div");
-    div.append($label);
-    div.append($input);
-    return div;
-  }
+	_createSelectBox(prefix, innerLabelText, stations) {
+		const $label = this._createLabel(
+			`${prefix}-station-select`,
+			innerLabelText
+		);
+		const $slectBox = document.createElement("select");
+		$slectBox.name = `${prefix}StationId`;
+		$slectBox.id = `${prefix}-station-select`;
+		const optionsHTML = stations
+			.map(
+				(station) => `<option value='${station.id}'>${station.name}</option>`
+			)
+			.join("");
 
-  _createSelectBox(prefix, labelInnerText, stations) {
-    const $label = document.createElement("label");
-    $label.htmlFor = `${prefix}-station-select`;
-    $label.innerText = labelInnerText;
-    const $slectBox = document.createElement("select");
-    $slectBox.name = `${prefix}StationId`;
-    $slectBox.id = `${prefix}-station-select`;
-    const optionsHTML = stations
-      .map(
-        (station) => `<option value='${station.id}'>${station.name}</option>`
-      )
-      .join("");
+		$slectBox.innerHTML = optionsHTML;
 
-    $slectBox.innerHTML = optionsHTML;
+		const div = document.createElement("div");
+		div.append($label);
+		div.append($slectBox);
+		return div;
+	}
 
-    const div = document.createElement("div");
-    div.append($label);
-    div.append($slectBox);
-    return div;
-  }
+	//공통
+	_createAddButton() {
+		const $addButton = document.createElement("button");
+		$addButton.id = "line-add-button";
+		$addButton.innerText = "노선 추가";
+		$addButton.type = "submit";
 
-  //공통
-  _createAddButton() {
-    const $addButton = document.createElement("button");
-    $addButton.id = "line-add-button";
-    $addButton.innerText = "노선 추가";
-    $addButton.type = "submit";
+		return $addButton;
+	}
 
-    return $addButton;
-  }
+	_createForm(stations) {
+		const $form = document.createElement("form");
+		$form.id = "line-form";
 
-  _createForm(stations) {
-    const $form = document.createElement("form");
-    $form.id = "line-form";
-    this.$form = $form;
+		const inputDiv = this._createInput();
+		const startSelectBox = this._createSelectBox(
+			"start",
+			"상행 노선",
+			stations
+		);
+		const endSelectBox = this._createSelectBox("end", "하행 노선", stations);
+		const submitButton = this._createAddButton();
 
-    const inputDiv = this._createInput();
-    const startSelectBox = this._createSelectBox(
-      "start",
-      "상행 종점",
-      stations
-    );
-    const endSelectBox = this._createSelectBox("end", "하행 종점", stations);
-    const submitButton = this._createAddButton();
+		$form.append(inputDiv);
+		$form.append(startSelectBox);
+		$form.append(endSelectBox);
+		$form.append(submitButton);
 
-    this.$form.append(inputDiv);
-    this.$form.append(startSelectBox);
-    this.$form.append(endSelectBox);
-    this.$form.append(submitButton);
+		return $form;
+	}
 
-    this.$linesTab.append(this.$form);
-    return this.$form;
-  }
-
-  render(stations) {
-    this._clearLinesTab();
-    this._createForm(stations);
-  }
+	render(stations) {
+		this._clearLinesTab();
+		const $form = this._createForm(stations);
+		this.$linesTab.append($form);
+	}
 }
--- src/view/StationView.js
+++ src/view/StationView.js
@@ -1,126 +1,127 @@
 export default class StationView {
-  constructor() {
-    // lazy load??
-    this.$stationsTab = null;
-    this.$form = null;
-    this.$input = null;
-    this.$table = null;
-    this.$tbody = null;
-  }
-  setStationsTab($stationsTab) {
-    this.$stationsTab = $stationsTab;
-  }
-  clearInputValue() {
-    if (this.$input) {
-      this.$input.value = "";
-    }
-  }
-  _clearTable() {
-    if (this.$table) {
-      this.$table.remove();
-    }
-  }
-  _clearStationsTab() {
-    if (this.$stationsTab) {
-      this.$stationsTab.innerHTML = "";
-    }
-  }
-  _createInput($form) {
-    const $label = document.createElement("label");
-    $label.htmlFor = "station-name-input";
-    $label.innerText = "역 이름";
+	constructor() {
+		// lazy load??
+		this.$stationsTab = null;
+		this.$form = null;
+		this.$input = null;
+		this.$table = null;
+		this.$tbody = null;
+	}
+	setStationsTab($stationsTab) {
+		this.$stationsTab = $stationsTab;
+	}
+	clearInputValue() {
+		if (this.$input) {
+			this.$input.value = "";
+		}
+	}
+	_clearTable() {
+		if (this.$table) {
+			this.$table.remove();
+		}
+	}
+	_clearStationsTab() {
+		if (this.$stationsTab) {
+			this.$stationsTab.innerHTML = "";
+		}
+	}
+	_createInput($form) {
+		const $label = document.createElement("label");
+		$label.htmlFor = "station-name-input";
+		$label.innerText = "역 이름";
 
-    const $input = document.createElement("input");
-    $input.id = "station-name-input";
-    $input.name = "name";
-    $input.placeholder = "역 이름을 입력해 주세요.";
-    this.$input = $input;
+		const $input = document.createElement("input");
+		$input.id = "station-name-input";
+		$input.name = "name";
+		$input.placeholder = "역 이름을 입력해 주세요.";
+		this.$input = $input;
 
-    $form.append($label);
-    $form.append($input);
-  }
+		$form.append($label);
+		$form.append($input);
+	}
 
-  _createAddButton($form) {
-    const $addButton = document.createElement("button");
-    $addButton.id = "station-add-button";
-    $addButton.innerText = "역 추가";
-    $addButton.type = "submit";
+	_createAddButton($form) {
+		const $addButton = document.createElement("button");
+		$addButton.id = "station-add-button";
+		$addButton.innerText = "역 추가";
+		$addButton.type = "submit";
 
-    $form.append($addButton);
-  }
-  _createForm() {
-    const $form = document.createElement("form");
-    $form.id = "station-form";
-    this.$form = $form;
+		$form.append($addButton);
+	}
+	_createForm() {
+		const $form = document.createElement("form");
+		$form.id = "station-form";
+		this.$form = $form;
 
-    this._createInput($form);
-    this._createAddButton($form);
+		this._createInput($form);
+		this._createAddButton($form);
 
-    this.$stationsTab.append($form);
-  }
-  _createTitle() {
-    const title = document.createElement("h1");
-    title.innerText = "지하철 역 목록";
-    this.$stationsTab.append(title);
-  }
+		this.$stationsTab.append($form);
+	}
+	_createTitle() {
+		const title = document.createElement("h1");
+		title.innerText = "지하철 역 목록";
+		this.$stationsTab.append(title);
+	}
 
-  _createTable() {
-    this.$table = document.createElement("table");
-  }
-  _createTHead() {
-    let tableHTML = "";
-    const header = "<thead><tr><th>역 이름</th><th>설정</th></tr></thead>";
-    tableHTML += header;
-    this.$table.innerHTML = tableHTML;
-  }
-  _createTBody(stations) {
-    const tbody = document.createElement("tbody");
-    tbody.innerHTML = stations
-      .map(
-        (station) =>
-          `<tr data-station-id='${station.id}'><td>${station.name}</td><td><button>삭제</button></td></tr>`
-      )
-      .join("");
+	_createTable() {
+		this.$table = document.createElement("table");
+	}
+	_createTHead() {
+		let tableHTML = "";
+		const header = "<thead><tr><th>역 이름</th><th>설정</th></tr></thead>";
+		tableHTML += header;
+		this.$table.innerHTML = tableHTML;
+	}
+	_createTBody(stations) {
+		const tbody = document.createElement("tbody");
+		tbody.innerHTML = stations
+			.map(
+				(station) =>
+					`<tr data-station-id='${station.id}'><td>${station.name}</td><td><button>삭제</button></td></tr>`
+			)
+			.join("");
 
-    this.$tbody = tbody;
-    this.$table.append(this.$tbody);
-  }
-  showTable(stations) {
-    this._clearTable();
-    this._createTable();
-    this._createTHead();
-    this._createTBody(stations);
-    this.$stationsTab.append(this.$table);
-  }
-  showStationsTab() {
-    this.$stationsTab.style.display = "block";
-  }
-  render(stations) {
-    this._clearStationsTab();
-    this._createForm();
-    this._createTitle();
-    this.showTable(stations);
-  }
-  // 사실 컨트롤러 단에서, tr객체를  넘겨줄 수도 있었다.
-  // 그렇게 하지 않은 이유는 컨트롤러는 데이터만 다루어야 하기 때문이라고 생각해서 이다.
-  // 하지만 그냥 tr객체를 보내서 지워도 된다면? 어떻게 해야할까.
-  removeTrByStationId(stationId) {
-    const tr = this.$tbody.querySelector(`[data-station-id='${stationId}']`);
-    if (tr) {
-      tr.remove();
-      // 왜 remove는 자기자신이 사라지는건가? yes -> 객체지향적인 동작.
-      //   this.$tbody.remove(tr);
-    }
-  }
+		this.$tbody = tbody;
+		this.$table.append(this.$tbody);
+	}
+	showTable(stations) {
+		this._clearTable();
+		this._createTable();
+		this._createTHead();
+		this._createTBody(stations);
+		this.$stationsTab.append(this.$table);
+	}
+	showStationsTab() {
+		this.$stationsTab.style.display = "block";
+	}
+	render(stations) {
+		this._clearStationsTab();
+		this._createForm();
+		this._createTitle();
+		this.showTable(stations);
+	}
+	// 사실 컨트롤러 단에서, tr객체를  넘겨줄 수도 있었다.
+	// 그렇게 하지 않은 이유는 컨트롤러는 데이터만 다루어야 하기 때문이라고 생각해서 이다.
+	// 하지만 그냥 tr객체를 보내서 지워도 된다면? 어떻게 해야할까.
+	removeTrByStationId(stationId) {
+		const tr = this.$tbody.querySelector(`[data-station-id='${stationId}']`);
+		if (tr) {
+			tr.remove();
+			// 왜 remove는 자기자신이 사라지는건가? yes -> 객체지향적인 동작.
+			//   this.$tbody.remove(tr);
+		}
+	}
 
-  // 여기서 제일 헷갈리는 파트는, bind시점에서 this.$form, this.$table이 없던 시점일지라도,
-  // 이벤트리스너가 잘 붙는다는 점이다. 왜지?
-  bindOnClickSubmit(onClickSublit) {
-    // const form = document.getElementById("station-name-form");
-    // 이렇게 하면... 이벤트 리스너는 객체주소에 붙는건가?
-    this.$form.addEventListener("submit", onClickSublit);
-  }
-  bindOnClickRemove(onClickRemove) {
-    this.$table.addEventListener("click", onClickRemove);
-  }
+	// 여기서 제일 헷갈리는 파트는, bind시점에서 this.$form, this.$table이 없던 시점일지라도,
+	// 이벤트리스너가 잘 붙는다는 점이다. 왜지?
+	bindOnClickSubmit(onClickSublit) {
+		// const form = document.getElementById("station-name-form");
+		// 이렇게 하면... 이벤트 리스너는 객체주소에 붙는건가?
+		this.$form.addEventListener("submit", onClickSublit);
+	}
+	bindOnClickRemove(onClickRemove) {
+		console.log("asdasd");
+		this.$table.addEventListener("click", onClickRemove);
+	}
 }
