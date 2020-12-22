1de6eaf794a5a80e0679af27e16f02c20b8c9e5d
이신호 <dltlsgh5@naver.com>
2020-12-22 22:27:25 +0900

    refactor: add LineModel

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
 import Controllor from "./controllor/Controllor.js";
 import StationModel from "./model/Station.js";
+import LineModel from "./model/Line.js";
 import BaseView from "./view/BaseView.js";
 import StationView from "./view/StationView.js";
 import LineView from "./view/LineView.js";
 
 class App {
-  $target = null;
-  constructor() {
-    this.$target = document.getElementById("app");
+	$target = null;
+	constructor() {
+		this.$target = document.getElementById("app");
 
-    new Controllor(this.createModels(), this.createViews());
-  }
+		new Controllor(this.createModels(), this.createViews());
+	}
 
-  createModels() {
-    const stationModel = new StationModel();
-    const models = { stationModel };
-    return models;
-  }
+	createModels() {
+		const stationModel = new StationModel();
+		const lineModel = new LineModel();
+		const models = { stationModel, lineModel };
+		return models;
+	}
 
-  createViews() {
-    const baseView = new BaseView(this.$target);
-    const stationView = new StationView();
-    const lineView = new LineView();
-    const views = { baseView, stationView, lineView };
-    return views;
-  }
+	createViews() {
+		const baseView = new BaseView(this.$target);
+		const stationView = new StationView();
+		const lineView = new LineView();
+		const views = { baseView, stationView, lineView };
+		return views;
+	}
 }
 
 export default new App();
