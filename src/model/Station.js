120a2785e7f69367c554a83065b5aadaa4347e3a
이신호 <dltlsgh5@naver.com>
2021-01-01 21:05:36 +0900

    refactor: id기준으로 객체 반환하는 로직 추가

--- src/model/Station.js
+++ src/model/Station.js
@@ -1,39 +1,49 @@
 import { setLocalStorage, getLocalStorageByKey } from "../../utils.js";
 
 export default class StationModel {
-  constructor() {
-    const stations = getLocalStorageByKey("stations");
-    if (!stations) {
-      setLocalStorage("stations", []);
-    }
-    this.stations = stations;
-  }
+	constructor() {
+		const stations = getLocalStorageByKey("stations");
+		if (!stations) {
+			setLocalStorage("stations", []);
+		}
+		this.stations = stations;
+	}
 
-  getStations() {
-    console.log("in getStations, ", this);
-    return getLocalStorageByKey("stations");
-  }
-  /**
-   * station = {
-   * @param name string
-   * @param id uuid
-   * }
-   * **/
+	getStations() {
+		console.log("in getStations, ", this);
+		return getLocalStorageByKey("stations");
+	}
+	/**
+	 * station = {
+	 * @param name string
+	 * @param id uuid
+	 * }
+	 * **/
+	getStationsByIds(stationIds) {
+		const stations = this.getStations();
+		return stations.filter((station) => !!~stationIds.indexOf(station.id));
+	}
 
-  addStation(station, callback) {
-    const stations = this.getStations();
-    const newStations = [...stations, { ...station }];
-    setLocalStorage("stations", newStations);
+	findStationById(stationId) {
+		const stations = this.getStations();
 
-    if (callback) callback();
-  }
-  /**
-   * @param id uuid
-   * **/
-  removeStationById(id, callback) {
-    const stations = this.getStations();
-    const newStations = stations.filter((station) => station.id !== id);
-    setLocalStorage("stations", newStations);
-    if (callback) callback();
-  }
+		return stations.find((station) => station.id === stationId);
+	}
+
+	addStation(station, callback) {
+		const stations = this.getStations();
+		const newStations = [...stations, { ...station }];
+		setLocalStorage("stations", newStations);
+
+		if (callback) callback();
+	}
+	/**
+	 * @param id uuid
+	 * **/
+	removeStationById(id, callback) {
+		const stations = this.getStations();
+		const newStations = stations.filter((station) => station.id !== id);
+		setLocalStorage("stations", newStations);
+		if (callback) callback();
+	}
 }
